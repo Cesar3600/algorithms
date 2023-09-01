748ec5fa3251dff20cfe9b0a89800f4bcc11fb9e
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-09-01 16:36:33 -0500

    fixed: agree binary way for power of two

--- powerOfTwo.ts
+++ powerOfTwo.ts
@@ -21,3 +21,14 @@ console.log(powerOfTwo(4))
 // 2^4 => 16
 // 2^5 => 32
 
+// another solution:
+
+interface powerOfTwoConf{
+  (n:number):boolean
+}
+
+const powerOfTwo2:powerOfTwoConf = n => {
+   return !(n & (n-1))
+}
+
+console.log(powerOfTwo2(4))
